7f5f49d4d45a81b3defd6f3decdba96a18d903c5
MikeDressler <mike.dressler@rwth-aachen.de>
2015-12-11 22:42:59 +0100

    first Commit
    Signed-off-by: MikeDressler <mike.dressler@rwth-aachen.de>

---
+++ js/_vti_cnf/bootstrap-image-gallery.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|24 Jul 2015 06:47:54 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_author:SR|Gigamike\\Mike
+vti_modifiedby:SR|Gigamike\\Mike
+vti_timecreated:TR|24 Jul 2015 06:47:54 -0000
+vti_cacheddtm:TX|24 Jul 2015 06:47:54 -0000
+vti_filesize:IR|2875
